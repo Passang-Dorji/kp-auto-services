ae6a960a68b81a4e06a060a0fa58d5b22056b381
Passang Dorji <pssngdor94@gmail.com>
2026-09-02 16:01:05 +0600

    fix: fixes layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Motivo Auto Care | Service & Genuine Parts",
-  description:
-    "Expert auto service, honest diagnostics, and trusted spare parts—all under one roof.",
+	title: "KP Auto Care | Service & Genuine Parts",
+	description:
+		"Expert auto service, honest diagnostics, and trusted spare parts—all under one roof.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
